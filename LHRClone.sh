285570a61ba8162d5f5500fe6e909eeb64a1d04f
Lakshay983 <lakshay45gupta@gmail.com>
2024-01-18 16:42:21 -0600

    Adding commands for KiCAD install

--- LHRClone.sh
+++ LHRClone.sh
@@ -13,21 +13,32 @@ function makeDirectory(){
 }
 
 function EmbeddedSharePoint(){
+    echo "Cloning the Embedded Sharepoint repository"
     git clone git@github.com:lhr-solar/Embedded-Sharepoint.git --recursive
     cd Embedded-Sharepoint/Scripts
+    echo "Running embedded install script"
     bash install.sh -Ii
     cd ..
     cd ..
 }
 
 function BPS(){
+    echo "Cloning the BPS repository"
     git clone git@github.com:lhr-solar/BPS.git --recursive
     cd ..
 }
 
 function Controls(){
+    echo "Cloning the Controls repository"
     git clone git@github.com:lhr-solar/Controls.git --recursive
     cd ..
+}
+
+function KiCAD(){
+    echo "Installing KiCAD"
+    sudo add-apt-repository --yes ppa:kicad/kicad-7.0-releases
+    sudo apt update
+    sudo apt install --install-recommends kicad
 }
 
 makeDirectory
@@ -39,3 +50,5 @@ EmbeddedSharePoint
 BPS
 
 Controls
+
+KiCAD
